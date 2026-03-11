3c0914129889f8edbdeba46af97bed309dcece1e
code-Afif <afifk1106@gmail.com>
2026-03-11 09:47:32 +0530

    loops and conditional statement

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("Hello World")
+      // document.write("Starting loops" + "<br>")
+      // for(count = 0;count<5;count++){
+      //   document.write("Current Count :" +count+"<br>")
+
+      // }
+
+      // 
+      // const person = {
+      //   fname :"Afif",
+      //   lname : "Khan",
+      //   age : 18
+      // }
+      // for (let x in person){
+      //   console.log("person Details"+ x + ":" + person[x])
+      // }
+
+      // while loop
+      // while(count<10){
+      //   document.write("cuurent count" + count + "<br>")
+      // }
+
+      // Concditional statement
+    //  If Else
+    //   var age = 20
+    //   if (age>18){
+    //     document.write("<b>you are eligible to cast your vote</b>");
+    //   }
+    //   else{
+    //     document.write("You are under 18 you cant cast your vote");
+    //   }
+
+    // switch case 
+    var grade = 'A';
+    switch(grade){
+        case 'A':
+            document.write("passed")
+            break;
+        case 'B':
+            document.write("failed")
+            break;
+        default:
+            document.write("invalid option detected")
+            break;
+        
+    }
